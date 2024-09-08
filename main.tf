6178949c88513e50ffeb30efce101d5f1559ed71
Eyoel Feleke <eyu2.wf@gmail.com>
2024-09-08 16:50:21 -0500

    feat: Add HTTPS inbound rule to AWS security group for blog

--- main.tf
+++ main.tf
@@ -46,6 +46,16 @@ resource "aws_security_group_rule" "blog_http_inbound" {
   security_group_id = aws_security_group.blog.id
 }
 
+resource "aws_security_group_rule" "blog_https_inbound" {
+  type          = "ingress"
+  from_port     = 443
+  to_port       = 443
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
 resource "aws_security_group_rule" "blog_everything_outbound" {
   type          = "egress"
   from_port     = 0
