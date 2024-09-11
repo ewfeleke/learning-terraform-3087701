746ac167edd44ccda11ac87e50872bdd274a759c
Eyoel Feleke <eyu2.wf@gmail.com>
2024-09-11 12:58:08 -0500

    Create a VPC using modules

--- main.tf
+++ main.tf
@@ -18,10 +18,11 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -32,7 +33,7 @@ resource "aws_instance" "web" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  vpc_id = data.aws_vpc.default.id 
+  vpc_id = module.blog_vpc.vpc_id 
   name = "blog_sg_new"
   description = "Allow http and https in. Allow all traffic out."
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
@@ -42,39 +43,17 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group" "blog" {
-   name = "blog"
-   description = "Allow http and https in. Allow all traffic out."
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-    vpc_id = data.aws_vpc.default.id 
-}
+  name = "dev-blog-vpc"
+  cidr = "10.0.0.0/16"
 
-resource "aws_security_group_rule" "blog_http_inbound" {
-  type          = "ingress"
-  from_port     = 80
-  to_port       = 80
-  protocol      = "tcp"
-  cidr_blocks   = ["0.0.0.0/0"]
+  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_inbound" {
-  type          = "ingress"
-  from_port     = 443
-  to_port       = 443
-  protocol      = "tcp"
-  cidr_blocks   = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_outbound" {
-  type          = "egress"
-  from_port     = 0
-  to_port       = 0
-  protocol      = -1
-  cidr_blocks   = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
