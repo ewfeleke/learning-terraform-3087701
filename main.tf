a94d7d21519de2e0ca07ae26c4587948e6e34c10
Eyoel Feleke <eyu2.wf@gmail.com>
2024-09-11 14:30:16 -0500

    feat: Update blog ALB module to use HTTP protocol and port 80

--- main.tf
+++ main.tf
@@ -42,8 +42,8 @@ module "blog_alb" {
   target_groups = [
     {
       name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
+      protocol = "HTTP"
+      port     = 80
       target_type      = "instance"
     }
   ]
