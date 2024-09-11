d89ad31b77c160d2830132dd7a4986dae996924a
Eyoel Feleke <eyu2.wf@gmail.com>
2024-09-11 15:16:01 -0500

    Update blog ALB module to use default action for forwarding

--- main.tf
+++ main.tf
@@ -34,41 +34,41 @@ resource "aws_instance" "blog" {
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
+  version = "~> 6.0"
+
   name    = "blog-alb"
+  
+  load_balancer_type = "application"
+  
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
 
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
-
-  target_groups = {
-    ex-instance = {
+  target_groups = [
+    {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      targets = [
+        {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      ]
     }
-  }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+
 
   tags = {
     Terraform = "true"
